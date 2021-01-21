a70e2a74a7e4d6609fcfe7acf13a3bbe89eaff95
Jonathan <jonathan.george.dev@gmail.com>
2021-01-20 22:21:27 -0800

    All set up and ready to code.

---
+++ src/index.js
@@ -0,0 +1,7 @@
+//contain code that inserts Reac App into our public index.html
+
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App.js";
+
+ReactDOM.render(<App />, document.getElementById("root")); //root is defined in public/index.html
